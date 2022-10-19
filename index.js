db04193eaa956ccbe0267a6a137c75d38e8f6e2e
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-10-18 17:45:08 -0700

    commit tree 4785e732151b44f7286ded6cb4657dfb193c3dce

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const sendData = {
+name: "Gerardo Rapeta",
+mail: "madeupemail@gmail.com"
+};
+
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({name: "bob"}),
+    
+};
+
+
+fetch("http://localhost:3000/users", submitData)
+.then((res)=>{
+        return res.json()
+    })
+.then((obj)=> console.log(obj))
+
+    
